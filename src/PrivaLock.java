9fbff14ccd4e1003de1782955891528b1355fe56
David Van <David@DavidVan.net>
2016-04-29 20:14:31 -0700

    Added main and default constructor for Settings.

--- src/PrivaLock.java
+++ src/PrivaLock.java
@@ -2,4 +2,7 @@
  * Created by knnth on 4/29/2016.
  */
 public class PrivaLock {
+    public static void main(String[] args) {
+
+    }
 }
--- src/Settings.java
+++ src/Settings.java
@@ -3,6 +3,8 @@
  */
 public class Settings {
 
+    public Settings() {
 
+    }
 
 }
