d3f33f351d5a83f0012aaf68b15793f0fcd523de
xxmarkspencerxx <macbonkaynel@gmail.com>
2016-04-30 11:58:43 -0700

    add gui

--- src/PrivaLock.java
+++ src/PrivaLock.java
@@ -38,6 +38,7 @@ public class PrivaLock {
                         encryption = new CryptoUtility(scan.next());
                         encryption.encrypt(myFile, encrypted);
                         myFile.delete();
+                        
                     }
                     break;
                 case 2:
