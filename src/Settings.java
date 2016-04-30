2b0a5f938d4f353a8f525753612114d5fad0a103
Ryan Ea <helloxhi1@gmail.com>
2016-04-30 00:08:58 -0700

    WIP settings

--- src/Settings.java
+++ src/Settings.java
@@ -1,12 +1,16 @@
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-
+import java.nio.file.Files;
 /**
  * Created by knnth on 4/29/2016.
  */
-public class Settings<V> {
+public class Settings implements Serializable{
 
     private String password = null;
-    private ArrayList<V> fileLock;
+    private ArrayList<String> fileLock = new ArrayList<String>();
     public Settings() {
         this.password = null;
     }
@@ -15,8 +19,13 @@ public class Settings<V> {
         this.password = password;
     }
 
-    public void save(){
-
+    public void save()throws IOException{
+        Path settings= Paths.get(System.getProperty("user.home")+"\\PrivaLock\\Setttings");
+        System.out.println("!!!");
+        System.out.println(settings.toString());
+        System.out.println("!!!");
+        if(!Files.exists(settings)){
+        }
         /**
          * You want to be able to save the current state of settings.. Implement here. If there was a setting left unmarked, will prompt error accordingly
          */
@@ -34,7 +43,7 @@ public class Settings<V> {
 
     }
 
-    public void chooseFiles(V file){
+    public void chooseFiles(String file){
         fileLock.add(file);
     }
 
