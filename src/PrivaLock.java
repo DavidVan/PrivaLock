8bd625f2a973fb69fe2ebedcdbe3778be1a6aa53
David Van <David@DavidVan.net>
2016-04-30 03:08:50 -0700

    Fixed merge commit and remove obvious unnecessary code.

--- src/PrivaLock.java
+++ src/PrivaLock.java
@@ -1,10 +1,11 @@
+
 /**
  * Created by knnth on 4/29/2016.
  */
 public class PrivaLock {
+
     public static void main(String[] args) {
-        Settings RyanEaPorn = new Settings("Ryan", "I love doge");
-        RyanEaPorn.save();
 
     }
+
 }
