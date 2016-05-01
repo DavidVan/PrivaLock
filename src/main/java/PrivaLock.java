f51c54da3d05d280deeda44ba5477ba437c4fa06
xxmarkspencerxx <macbonkaynel@gmail.com>
2016-04-30 17:03:29 -0700

    main stuff

--- src/main/java/PrivaLock.java
+++ src/main/java/PrivaLock.java
@@ -16,19 +16,18 @@ public class PrivaLock {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Password for Encryption: ");
         encryption = new CryptoUtility(scan.next());
-        System.out.println("\n\n\n\n\n\n");
-        System.out.println("PrivaLock Menu:\n" +
-                "1) Choose Files to Encrypt\n" +
-                "2) Choose Files to Decrypt\n" +
-                "3) Settings\n" +
-                "4) Exit\n");
+        System.out.println("\n\n");
+
         int option = 0;
         while (option != 4){
+            System.out.println("\nPrivaLock Menu:\n" +
+                    "1) Choose Files to Encrypt\n" +
+                    "2) Choose Files to Decrypt\n" +
+                    "3) Settings\n" +
+                    "4) Exit\n");
             option = scan.nextInt();
 
-
             switch (option){
-
                 case 1:
                     fileChooser.showOpenDialog(null);
                     File[] uploadDir = fileChooser.getSelectedFiles();
@@ -37,7 +36,7 @@ public class PrivaLock {
                         File myFile = new File(file);
                         File encrypted = new File(file + ".encrypted");
                         encryption.encrypt(myFile, encrypted);
-                        
+                        System.out.println("Successfully Encrypted: " + file);
                     }
                     break;
                 case 2:
@@ -52,7 +51,6 @@ public class PrivaLock {
                         AuthenticationObject name = new AuthenticationObject(test.digest());
                         File decrypted = new File(file.substring(0,file.lastIndexOf('.')));
                         encryption.decrypt(name.getData(),myFile, decrypted);
-
                     }
                     break;
                 case 3:
